swgemu/Core3

[Fixed] Mantis 8139: Crate types will now be based on what is in their Lua Template.
Change-Id: Iac033bedb618b3e236f5a7a660ccec698675aaf9

MMOCoreORB/src/server/zone/objects/draftschematic/DraftSchematicImplementation.cpp
@@ -176,3 +176,7 @@ int DraftSchematicImplementation::getLabratory() {
 int DraftSchematicImplementation::getFactoryCrateSize() {
 	return schematicTemplate->getFactoryCrateSize();
 }
+
+String DraftSchematicImplementation::getCrateType() {
+	return schematicTemplate->getCrateType();
+}

MMOCoreORB/src/templates/intangible/DraftSchematicObjectTemplate.cpp
@@ -73,6 +73,8 @@ void DraftSchematicObjectTemplate::parseVariableData(const String& varName, LuaO
 		xp = Lua::getIntParameter(state);
 	} else if (varName == "lab") {
 		labratory = Lua::getIntParameter(state);
+	} else if (varName == "crateType") {
+		crateType = Lua::getStringParameter(state);
 	} else if (varName == "factoryCrateSize") {
 		factoryCrateSize = Lua::getIntParameter(state);
 	} else if (varName == "isMagic") {

swgsremu/sentinelsrepublic-server

MMOCoreORB/src/server/zone/objects/draftschematic/DraftSchematicImplementation.cpp
@@ -176,3 +176,7 @@ int DraftSchematicImplementation::getLabratory() {
 int DraftSchematicImplementation::getFactoryCrateSize() {
 	return schematicTemplate->getFactoryCrateSize();
 }
+
+String DraftSchematicImplementation::getFactoryCrateType() {
+	return schematicTemplate->getFactoryCrateType();
+}

MMOCoreORB/src/templates/intangible/DraftSchematicObjectTemplate.cpp
@@ -73,6 +73,8 @@ void DraftSchematicObjectTemplate::parseVariableData(const String& varName, LuaO
 		xp = Lua::getIntParameter(state);
 	} else if (varName == "lab") {
 		labratory = Lua::getIntParameter(state);
+	} else if (varName == "factoryCrateType") {
+		factoryCrateType = Lua::getStringParameter(state);
 	} else if (varName == "factoryCrateSize") {
 		factoryCrateSize = Lua::getIntParameter(state);
 	} else if (varName == "isMagic") {