swgemu/Core3

[added] Support for managing quest journal for post JTL quests
Change-Id: Ief743db2b111c9cae017d0941e9cb59dd0084877

MMOCoreORB/src/server/zone/objects/player/LuaPlayerObject.h
@@ -57,6 +57,16 @@ namespace player {
 		int setJediState(lua_State* L);
 		int getJediState(lua_State* L);
 		int isOnline(lua_State* L);
+		int questActivateQuest(lua_State* L);
+		int questCompleteQuest(lua_State* L);
+		int questClearQuest(lua_State* L);
+		int questActivateQuestTask(lua_State* L);
+		int questCompleteQuestTask(lua_State* L);
+		int questClearQuestTask(lua_State* L);
+		int questIsQuestActive(lua_State* L);
+		int questIsQuestComplete(lua_State* L);
+		int questIsQuestTaskActive(lua_State* L);
+		int questIsQuestTaskComplete(lua_State* L);
 		int setActiveQuestsBit(lua_State* L);
 		int clearActiveQuestsBit(lua_State* L);
 		int hasActiveQuestBitSet(lua_State* L);

swgsremu/sentinelsrepublic-server

MMOCoreORB/src/server/zone/objects/player/LuaPlayerObject.h
@@ -57,6 +57,16 @@ namespace player {
 		int setJediState(lua_State* L);
 		int getJediState(lua_State* L);
 		int isOnline(lua_State* L);
+		int activateJournalQuest(lua_State* L);
+		int completeJournalQuest(lua_State* L);
+		int clearJournalQuest(lua_State* L);
+		int activateJournalQuestTask(lua_State* L);
+		int completeJournalQuestTask(lua_State* L);
+		int clearJournalQuestTask(lua_State* L);
+		int isJournalQuestActive(lua_State* L);
+		int isJournalQuestComplete(lua_State* L);
+		int isJournalQuestTaskActive(lua_State* L);
+		int isJournalQuestTaskComplete(lua_State* L);
 		int setActiveQuestsBit(lua_State* L);
 		int clearActiveQuestsBit(lua_State* L);
 		int hasActiveQuestBitSet(lua_State* L);