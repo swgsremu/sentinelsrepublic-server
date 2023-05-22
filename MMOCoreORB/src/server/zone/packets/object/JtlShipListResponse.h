swgemu/Core3

[Added] destroyObjectFromDatabase to shipObject and ship control device
[Added] Player ship chassis to character builder terminal
[Added] Launch/Land ship options to ship control device
[Added] Insert/Remove player to ship control device
[Fixed] Ship management terminal response and fast travel
[Fixed] Stability issue with invalid StringIDs
[Fixed] Stability issue with spaceStation conversation handler

Change-Id: I50fc1b8b1b2238789674cd18f0b147d7b7aa1ea4

MMOCoreORB/src/server/zone/packets/object/JtlShipListResponse.h
@@ -11,49 +11,77 @@
 #include "ObjectControllerMessage.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/ship/ShipObject.h"
+#include "server/zone/objects/intangible/ShipControlDevice.h"
 
-//TODO: This is very unsafe still...
-class JtlShipListResponse : public ObjectControllerMessage {
+class JtlShipListResponse: public ObjectControllerMessage {
 public:
-	JtlShipListResponse(CreatureObject* creo, SceneObject* terminal)
-		: ObjectControllerMessage(creo->getObjectID(), 0x1B, 0x41D) {
-
-
-			PlayerObject* ghost = creo->getPlayerObject();
-			if (ghost == nullptr)
-				return;
-
-			int numShips = 0;
-			Vector<Reference<ShipObject*>> ships;
-
-			for (int i=0; i<ghost->getNumShips(); i++) {
-				ManagedReference<ShipObject*> ship = ghost->getShip(i);
-
-				if (ship != nullptr)
-					ships.add(ship);
-			}
-
-			insertInt(ships.size()+1); // Number of ships
-			insertLong(terminal->getObjectID()); // Space Terminal ID
-
-			String currentCity;
-
-			if (creo->getCityRegion().get() != nullptr)
-				currentCity = creo->getCityRegion().get()->getRegionDisplayedName();
-			else
-				currentCity = creo->getZone()->getZoneName();
-
-			insertAscii(currentCity); //Player Location
-
-			for (auto &ship : ships) {
-				insertLong(ship->getObjectID());
-				String parked = ship->getStoredLocation();
-				if (parked == "")
-					ship->setStoredLocation(currentCity);
-
-				insertAscii(parked);
-			}
+	JtlShipListResponse(CreatureObject* creo, SceneObject* terminal) : ObjectControllerMessage(creo->getObjectID(), 0x1B, 0x41D) {
+		PlayerObject* ghost = creo->getPlayerObject();
+		if (ghost == nullptr) {
+			return;
 		}
+
+		auto datapad = creo->getSlottedObject("datapad");
+		if (datapad == nullptr) {
+			return;
+		}
+
+		auto zone = creo->getZone();
+		if (zone == nullptr) {
+			return;
+		}
+
+		auto planetManager = zone->getPlanetManager();
+		if (planetManager == nullptr) {
+			return;
+		}
+
+		auto travelPoint = planetManager->getNearestPlanetTravelPoint(creo->getWorldPosition(), FLT_MAX);
+		if (travelPoint == nullptr) {
+			return;
+		}
+
+		VectorMap<String, uint64> shipMap;
+
+		for (int i = 0; i < datapad->getContainerObjectsSize(); ++i) {
+			ManagedReference<ShipControlDevice*> control = datapad->getContainerObject(i).castTo<ShipControlDevice*>();
+			if (control == nullptr) {
+				continue;
+			}
+
+			auto object = control->getControlledObject();
+			if (object == nullptr || !object->isShipObject()) {
+				continue;
+			}
+
+			if (control->getStoredCityName() == "") {
+				control->setStoredLocationData(creo);
+			}
+
+			shipMap.put(control->getStoredCityName(), object->getObjectID());
+		}
+
+		auto travelPointName = travelPoint->getPointName();
+		if (travelPointName == "") {
+			travelPointName = zone->getZoneName();
+		}
+
+		insertInt(shipMap.size() + 1); // Number of ships
+		insertLong(terminal->getObjectID()); // Space Terminal ID
+		insertAscii(travelPointName); //Player Location
+
+		for (int i = 0; i < shipMap.size(); ++i) {
+			auto cityName = shipMap.elementAt(i).getKey();
+			auto shipID = shipMap.elementAt(i).getValue();
+
+			if (cityName == "") {
+				cityName = travelPointName;
+			}
+
+			insertLong(shipID);
+			insertAscii(cityName);
+		}
+	}
 };
 
 #endif /* JTLSHIPLISTRESPONSE_H_ */

swgsremu/sentinelsrepublic-server

MMOCoreORB/src/server/zone/packets/object/JtlShipListResponse.h
@@ -11,49 +11,86 @@
 #include "ObjectControllerMessage.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/ship/ShipObject.h"
+#include "server/zone/objects/intangible/ShipControlDevice.h"
 
-//TODO: This is very unsafe still...
-class JtlShipListResponse : public ObjectControllerMessage {
+class JtlShipListResponse: public ObjectControllerMessage {
 public:
-	JtlShipListResponse(CreatureObject* creo, SceneObject* terminal)
-		: ObjectControllerMessage(creo->getObjectID(), 0x1B, 0x41D) {
+	JtlShipListResponse(CreatureObject* creo, SceneObject* terminal) : ObjectControllerMessage(creo->getObjectID(), 0x1B, 0x41D) {
+		PlayerObject* ghost = creo->getPlayerObject();
 
-
-			PlayerObject* ghost = creo->getPlayerObject();
-			if (ghost == nullptr)
-				return;
-
-			int numShips = 0;
-			Vector<Reference<ShipObject*>> ships;
-
-			for (int i=0; i<ghost->getNumShips(); i++) {
-				ManagedReference<ShipObject*> ship = ghost->getShip(i);
-
-				if (ship != nullptr)
-					ships.add(ship);
-			}
-
-			insertInt(ships.size()+1); // Number of ships
-			insertLong(terminal->getObjectID()); // Space Terminal ID
-
-			String currentCity;
-
-			if (creo->getCityRegion().get() != nullptr)
-				currentCity = creo->getCityRegion().get()->getRegionDisplayedName();
-			else
-				currentCity = creo->getZone()->getZoneName();
-
-			insertAscii(currentCity); //Player Location
-
-			for (auto &ship : ships) {
-				insertLong(ship->getObjectID());
-				String parked = ship->getStoredLocation();
-				if (parked == "")
-					ship->setStoredLocation(currentCity);
-
-				insertAscii(parked);
-			}
+		if (ghost == nullptr) {
+			return;
 		}
+
+		auto datapad = creo->getSlottedObject("datapad");
+
+		if (datapad == nullptr) {
+			return;
+		}
+
+		auto zone = creo->getZone();
+
+		if (zone == nullptr) {
+			return;
+		}
+
+		auto planetManager = zone->getPlanetManager();
+
+		if (planetManager == nullptr) {
+			return;
+		}
+
+		auto travelPoint = planetManager->getNearestPlanetTravelPoint(creo->getWorldPosition(), FLT_MAX);
+
+		if (travelPoint == nullptr) {
+			return;
+		}
+
+		VectorMap<uint64, String> shipMap;
+
+		for (int i = 0; i < datapad->getContainerObjectsSize(); ++i) {
+			ManagedReference<ShipControlDevice*> control = datapad->getContainerObject(i).castTo<ShipControlDevice*>();
+
+			if (control == nullptr) {
+				continue;
+			}
+
+			auto object = control->getControlledObject();
+
+			if (object == nullptr || !object->isShipObject()) {
+				continue;
+			}
+
+			if (control->getStoredCityName() == "") {
+				Locker cLock(creo);
+				control->setStoredLocationData(creo);
+			}
+
+			shipMap.put(object->getObjectID(), control->getStoredCityName());
+		}
+
+		auto travelPointName = travelPoint->getPointName();
+
+		if (travelPointName == "") {
+			travelPointName = zone->getZoneName();
+		}
+
+		insertInt(shipMap.size() + 1); // Number of ships
+		insertLong(terminal->getObjectID()); // Space Terminal ID
+		insertAscii(travelPointName); //Player Location
+
+		for (int i = 0; i < shipMap.size(); ++i) {
+			auto shipID = shipMap.elementAt(i).getKey();
+			auto cityName = shipMap.elementAt(i).getValue();
+
+			if (cityName == "") {
+				cityName = travelPointName;
+			}
+
+			insertLong(shipID);
+			insertAscii(cityName);
+		}
+	}
 };
 
 #endif /* JTLSHIPLISTRESPONSE_H_ */