swgemu/Core3

[added] Ai space aggro and combat [updated] ShipAi movement by Smoki
Change-Id: Ic7c641055e9f77801d0cdd70cb74912cc2dd920a

MMOCoreORB/src/server/zone/managers/space/SpaceAiMap.h
@@ -121,6 +121,7 @@ public:
 		lua->setGlobalInt("AGGROSPACE",			BehaviorTreeSlotSpace::AGGROSPACE);
 		lua->setGlobalInt("ATTACKSPACE",		BehaviorTreeSlotSpace::ATTACKSPACE);
 		lua->setGlobalInt("AWARESPACE",			BehaviorTreeSlotSpace::AWARESPACE);
+		lua->setGlobalInt("EVADESPACE",			BehaviorTreeSlotSpace::EVADESPACE);
 		lua->setGlobalInt("IDLESPACE",			BehaviorTreeSlotSpace::IDLESPACE);
 		lua->setGlobalInt("LOOKATSPACE",		BehaviorTreeSlotSpace::LOOKATSPACE);
 		lua->setGlobalInt("MOVESPACE",			BehaviorTreeSlotSpace::MOVESPACE);
@@ -266,6 +267,13 @@ private:
 		_REGISTERSPACELEAF(CheckProspectInRange);
 		_REGISTERSPACELEAF(CheckAggroDelayPast);
 		_REGISTERSPACELEAF(CheckHasFollow);
+		_REGISTERSPACELEAF(CheckProspectAggression);
+		_REGISTERSPACELEAF(CheckRefireRate);
+		_REGISTERSPACELEAF(CheckEvadeDelayPast);
+		_REGISTERSPACELEAF(CheckTargetIsValid);
+		_REGISTERSPACELEAF(CheckEnginesDisabled);
+		_REGISTERSPACELEAF(CheckEvadeChance);
+		_REGISTERSPACELEAF(CheckRetreat);
 
 		// action behaviors
 		_REGISTERSPACELEAF(DummySpace);
@@ -277,9 +285,13 @@ private:
 		_REGISTERSPACELEAF(EraseBlackboard);
 		_REGISTERSPACELEAF(CalculateAggroMod);
 		_REGISTERSPACELEAF(SetMovementState);
-		//_REGISTERSPACELEAF(Evade); TODO Implement Evade
+		_REGISTERSPACELEAF(Evade);
 		_REGISTERSPACELEAF(SetAlert);
 		_REGISTERSPACELEAF(SetDefenderFromProspect);
+		_REGISTERSPACELEAF(GetProspectFromThreatMap);
+		_REGISTERSPACELEAF(EngageTarget);
+		_REGISTERSPACELEAF(UpdateDisabledEngineSpeed);
+		_REGISTERSPACELEAF(Leash);
 	}
 
 	void putBitmask(Lua* lua, String key) {

MMOCoreORB/src/server/zone/objects/ship/ai/btspace/leafspace/SimpleActionsSpace.h
@@ -35,14 +35,9 @@ public:
 	}
 
 	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
-		DataValSpace mode = DataValSpace::FLYING;
-
-		//if (agent->peekBlackboard("moveMode"))
-		//	mode = static_cast<DataValSpace>(agent->readBlackboard("moveMode").get<uint32>());
-
 		uint32 movementState = agent->getMovementState();
 
-		if (movementState == ShipAiAgent::FOLLOWING) {
+		if (movementState == ShipAiAgent::FOLLOWING || movementState == ShipAiAgent::ATTACKING) {
 			return agent->findNextPosition(agent->getMaxDistance()) ? SUCCESS : FAILURE;
 		}
 
@@ -126,7 +121,7 @@ public:
 	}
 
 	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
-		return agent->generatePatrol(numPoints, distFromHome) ? SUCCESS : FAILURE;
+		return agent->generatePatrol(numPoints, agent->getEngineMaxSpeed() * 5.f) ? SUCCESS : FAILURE;
 	}
 
 	String print() const {
@@ -144,44 +139,32 @@ private:
 
 class ExitCombat : public BehaviorSpace {
 public:
-	ExitCombat(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args), clearDefenders(false) {
-		parseArgs(args);
+	ExitCombat(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args) {
 	}
 
-	ExitCombat(const ExitCombat& a) : BehaviorSpace(a), clearDefenders(a.clearDefenders) {
+	ExitCombat(const ExitCombat& a) : BehaviorSpace(a) {
 	}
 
 	ExitCombat& operator=(const ExitCombat& a) {
 		if (this == &a)
 			return *this;
 		BehaviorSpace::operator=(a);
-		clearDefenders = a.clearDefenders;
 		return *this;
 	}
 
-	void parseArgs(const LuaObject& args) {
-		clearDefenders = getArg<bool>()(args, "clearDefenders");
-	}
-
 	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
-		//if (!agent->isInCombat())
-		//	return FAILURE;
+		agent->removeDefenders();
+		agent->setTargetShipObject(nullptr);
 
-		//agent->clearCombatState(clearDefenders);
-
-		//return !agent->isInCombat() ? SUCCESS : FAILURE;
-		return FAILURE;
+		return SUCCESS;
 	}
 
 	String print() const {
 		StringBuffer msg;
-		msg << className << "-" << clearDefenders;
+		msg << className << "- called";
 
 		return msg.toString();
 	}
-
-private:
-	bool clearDefenders;
 };
 
 class WriteBlackboard : public BehaviorSpace {
@@ -279,19 +262,21 @@ public:
 
 class SetAlert : public BehaviorSpace {
 public:
-	SetAlert(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args), duration(0.f), show(true) {
+	SetAlert(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args), duration(0.f), aggroDelay(0.f) {
 		parseArgs(args);
 	}
 
-	SetAlert(const SetAlert& b) : BehaviorSpace(b), duration(b.duration), show(b.show) {
+	SetAlert(const SetAlert& b) : BehaviorSpace(b), duration(b.duration), aggroDelay(b.aggroDelay) {
 	}
 
 	SetAlert& operator=(const SetAlert& b) {
 		if (this == &b)
 			return *this;
+
 		BehaviorSpace::operator=(b);
+
 		duration = b.duration;
-		show = b.show;
+		aggroDelay = b.aggroDelay;
 		return *this;
 	}
 
@@ -308,17 +293,7 @@ public:
 
 		if (delay != nullptr && delay->isPast()) {
 			delay->updateToCurrentTime();
-			uint32 newDelay = 5000;
-			delay->addMiliTime(newDelay);
-		}
-
-		ManagedReference<SceneObject*> target = nullptr;
-
-		if (agent->peekBlackboard("targetProspect"))
-			target = agent->readBlackboard("targetProspect").get<ManagedReference<SceneObject*>>().get();
-
-		if (show && target != nullptr && target->isPlayerCreature()) {
-			//agent->sendReactionChat(target, ReactionManager::ALERT);
+			delay->addMiliTime(aggroDelay);
 		}
 
 		return SUCCESS;
@@ -326,19 +301,33 @@ public:
 
 	void parseArgs(const LuaObject& args) {
 		duration = (int)(getArg<float>()(args, "duration") * 1000);
-		show = getArg<bool>()(args, "show");
+		aggroDelay = (int)(getArg<float>()(args, "aggroDelay") * 1000);
 	}
 
 	String print() const {
 		StringBuffer msg;
-		msg << className << "-" << duration << ":" << show;
+		msg << className << "- Duration: " << duration << ", Aggro Delay: " << aggroDelay;
 
 		return msg.toString();
 	}
 
 private:
 	int duration;
-	bool show;
+	int aggroDelay;
+};
+
+class UpdateDisabledEngineSpeed : public BehaviorSpace {
+public:
+	UpdateDisabledEngineSpeed(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args) {
+	}
+
+	UpdateDisabledEngineSpeed(const FindNextPosition& a) : BehaviorSpace(a) {
+	}
+
+	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
+
+		return agent->setDisabledEngineSpeed() ? RUNNING : SUCCESS;
+	}
 };
 
 } // namespace leafspace

swgsremu/sentinelsrepublic-server

MMOCoreORB/src/server/zone/managers/space/SpaceAiMap.h
@@ -121,6 +121,7 @@ public:
 		lua->setGlobalInt("AGGROSPACE",			BehaviorTreeSlotSpace::AGGROSPACE);
 		lua->setGlobalInt("ATTACKSPACE",		BehaviorTreeSlotSpace::ATTACKSPACE);
 		lua->setGlobalInt("AWARESPACE",			BehaviorTreeSlotSpace::AWARESPACE);
+		lua->setGlobalInt("EVADESPACE",			BehaviorTreeSlotSpace::EVADESPACE);
 		lua->setGlobalInt("IDLESPACE",			BehaviorTreeSlotSpace::IDLESPACE);
 		lua->setGlobalInt("LOOKATSPACE",		BehaviorTreeSlotSpace::LOOKATSPACE);
 		lua->setGlobalInt("MOVESPACE",			BehaviorTreeSlotSpace::MOVESPACE);
@@ -237,7 +238,7 @@ public:
 	}
 
 private:
-	static const bool DEBUG_MODE = true;
+	static const bool DEBUG_MODE = false;
 	SpaceBehaviorFactory factory;
 
 	void registerBehaviors() {
@@ -266,6 +267,13 @@ private:
 		_REGISTERSPACELEAF(CheckProspectInRange);
 		_REGISTERSPACELEAF(CheckAggroDelayPast);
 		_REGISTERSPACELEAF(CheckHasFollow);
+		_REGISTERSPACELEAF(CheckProspectAggression);
+		_REGISTERSPACELEAF(CheckRefireRate);
+		_REGISTERSPACELEAF(CheckEvadeDelayPast);
+		_REGISTERSPACELEAF(CheckTargetIsValid);
+		_REGISTERSPACELEAF(CheckEnginesDisabled);
+		_REGISTERSPACELEAF(CheckEvadeChance);
+		_REGISTERSPACELEAF(CheckRetreat);
 
 		// action behaviors
 		_REGISTERSPACELEAF(DummySpace);
@@ -277,9 +285,14 @@ private:
 		_REGISTERSPACELEAF(EraseBlackboard);
 		_REGISTERSPACELEAF(CalculateAggroMod);
 		_REGISTERSPACELEAF(SetMovementState);
-		//_REGISTERSPACELEAF(Evade); TODO Implement Evade
+		_REGISTERSPACELEAF(Evade);
 		_REGISTERSPACELEAF(SetAlert);
 		_REGISTERSPACELEAF(SetDefenderFromProspect);
+		_REGISTERSPACELEAF(GetProspectFromThreatMap);
+		_REGISTERSPACELEAF(EngageTarget);
+		_REGISTERSPACELEAF(SetDisabledEngineSpeed);
+		_REGISTERSPACELEAF(Leash);
+		_REGISTERSPACELEAF(GetProspectFromDefenders);
 	}
 
 	void putBitmask(Lua* lua, String key) {

MMOCoreORB/src/server/zone/objects/ship/ai/btspace/leafspace/SimpleActionsSpace.h
@@ -35,14 +35,9 @@ public:
 	}
 
 	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
-		DataValSpace mode = DataValSpace::FLYING;
-
-		//if (agent->peekBlackboard("moveMode"))
-		//	mode = static_cast<DataValSpace>(agent->readBlackboard("moveMode").get<uint32>());
-
 		uint32 movementState = agent->getMovementState();
 
-		if (movementState == ShipAiAgent::FOLLOWING) {
+		if (movementState == ShipAiAgent::FOLLOWING || movementState == ShipAiAgent::ATTACKING) {
 			return agent->findNextPosition(agent->getMaxDistance()) ? SUCCESS : FAILURE;
 		}
 
@@ -126,7 +121,7 @@ public:
 	}
 
 	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
-		return agent->generatePatrol(numPoints, distFromHome) ? SUCCESS : FAILURE;
+		return agent->generatePatrol(numPoints, agent->getEngineMaxSpeed() * 5.f) ? SUCCESS : FAILURE;
 	}
 
 	String print() const {
@@ -144,44 +139,32 @@ private:
 
 class ExitCombat : public BehaviorSpace {
 public:
-	ExitCombat(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args), clearDefenders(false) {
-		parseArgs(args);
+	ExitCombat(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args) {
 	}
 
-	ExitCombat(const ExitCombat& a) : BehaviorSpace(a), clearDefenders(a.clearDefenders) {
+	ExitCombat(const ExitCombat& a) : BehaviorSpace(a) {
 	}
 
 	ExitCombat& operator=(const ExitCombat& a) {
 		if (this == &a)
 			return *this;
 		BehaviorSpace::operator=(a);
-		clearDefenders = a.clearDefenders;
 		return *this;
 	}
 
-	void parseArgs(const LuaObject& args) {
-		clearDefenders = getArg<bool>()(args, "clearDefenders");
-	}
-
 	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
-		//if (!agent->isInCombat())
-		//	return FAILURE;
+		agent->removeDefenders();
+		agent->setTargetShipObject(nullptr);
 
-		//agent->clearCombatState(clearDefenders);
-
-		//return !agent->isInCombat() ? SUCCESS : FAILURE;
-		return FAILURE;
+		return SUCCESS;
 	}
 
 	String print() const {
 		StringBuffer msg;
-		msg << className << "-" << clearDefenders;
+		msg << className << "- called";
 
 		return msg.toString();
 	}
-
-private:
-	bool clearDefenders;
 };
 
 class WriteBlackboard : public BehaviorSpace {
@@ -279,19 +262,21 @@ public:
 
 class SetAlert : public BehaviorSpace {
 public:
-	SetAlert(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args), duration(0.f), show(true) {
+	SetAlert(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args), duration(0.f), aggroDelay(0.f) {
 		parseArgs(args);
 	}
 
-	SetAlert(const SetAlert& b) : BehaviorSpace(b), duration(b.duration), show(b.show) {
+	SetAlert(const SetAlert& b) : BehaviorSpace(b), duration(b.duration), aggroDelay(b.aggroDelay) {
 	}
 
 	SetAlert& operator=(const SetAlert& b) {
 		if (this == &b)
 			return *this;
+
 		BehaviorSpace::operator=(b);
+
 		duration = b.duration;
-		show = b.show;
+		aggroDelay = b.aggroDelay;
 		return *this;
 	}
 
@@ -308,17 +293,7 @@ public:
 
 		if (delay != nullptr && delay->isPast()) {
 			delay->updateToCurrentTime();
-			uint32 newDelay = 5000;
-			delay->addMiliTime(newDelay);
-		}
-
-		ManagedReference<SceneObject*> target = nullptr;
-
-		if (agent->peekBlackboard("targetProspect"))
-			target = agent->readBlackboard("targetProspect").get<ManagedReference<SceneObject*>>().get();
-
-		if (show && target != nullptr && target->isPlayerCreature()) {
-			//agent->sendReactionChat(target, ReactionManager::ALERT);
+			delay->addMiliTime(aggroDelay);
 		}
 
 		return SUCCESS;
@@ -326,19 +301,33 @@ public:
 
 	void parseArgs(const LuaObject& args) {
 		duration = (int)(getArg<float>()(args, "duration") * 1000);
-		show = getArg<bool>()(args, "show");
+		aggroDelay = (int)(getArg<float>()(args, "aggroDelay") * 1000);
 	}
 
 	String print() const {
 		StringBuffer msg;
-		msg << className << "-" << duration << ":" << show;
+		msg << className << "- Duration: " << duration << ", Aggro Delay: " << aggroDelay;
 
 		return msg.toString();
 	}
 
 private:
 	int duration;
-	bool show;
+	int aggroDelay;
+};
+
+class SetDisabledEngineSpeed : public BehaviorSpace {
+public:
+	SetDisabledEngineSpeed(const String& className, const uint32 id, const LuaObject& args) : BehaviorSpace(className, id, args) {
+	}
+
+	SetDisabledEngineSpeed(const FindNextPosition& a) : BehaviorSpace(a) {
+	}
+
+	BehaviorSpace::Status execute(ShipAiAgent* agent, unsigned int startIdx = 0) const {
+
+		return agent->setDisabledEngineSpeed() ? RUNNING : SUCCESS;
+	}
 };
 
 } // namespace leafspace