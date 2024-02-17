swgemu/Core3

[fixed] isNearBank now accounts for bank terminals in faction bases and structures, with parent ID checks to ensure same cell
Change-Id: I1999f4c08af65c89e5341913778d453c322a588f

MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
@@ -2187,29 +2187,41 @@ String SceneObjectImplementation::getGameObjectTypeStringID() {
 }
 
 bool SceneObjectImplementation::isNearBank() {
-	Zone* zone = getZone();
 
-	if (zone != nullptr) {
-		SortedVector<QuadTreeEntry* > closeObjects;
-		zone->getInRangeObjects(getPositionX(), getPositionY(), 15.f, &closeObjects, true, false);
+	SortedVector<ManagedReference<QuadTreeEntry*> > closeObjects;
+	CloseObjectsVector* closeObjectsVector = (CloseObjectsVector*) getCloseObjects();
 
-		bool nearBank = false;
-
-		for (int i = 0; i < closeObjects.size(); ++i) {
-			SceneObject* sceneO = cast<SceneObject*>(closeObjects.get(i));
-
-			if (sceneO == nullptr)
-				continue;
-
-			if (sceneO->getGameObjectType() == SceneObjectType::BANK) {
-				nearBank = true;
-				break;
-			}
+	if (closeObjectsVector != nullptr) {
+		closeObjectsVector->safeCopyTo(closeObjects);
+	} else {
+		auto zone = getZone();
+		if (zone != nullptr) {
+			zone->getInRangeObjects(getWorldPositionX(), getWorldPositionY(), 15.f, &closeObjects, true, true);
+		} else {
+			return false;
 		}
+	}
 
-		if (nearBank) {
-			return true;
-		}
+	uint64 parentID = getParentID();
+
+	for (int i = 0; i < closeObjects.size(); ++i) {
+
+		ManagedReference<SceneObject*> sceneO = cast<SceneObject*>(closeObjects.get(i).get());
+
+		if (sceneO == nullptr)
+			continue;
+
+		if (sceneO->getParentID() != parentID)
+			continue;
+
+		if (sceneO->getGameObjectType() != SceneObjectType::BANK)
+			continue;
+
+		if (closeObjectsVector != nullptr && getDistanceTo(sceneO) > 15.f) 
+			continue;
+
+		return true;
+
 	}
 
 	return false;

swgsremu/sentinelsrepublic-server

MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
@@ -2422,29 +2422,43 @@ String SceneObjectImplementation::getGameObjectTypeStringID() {
 }
 
 bool SceneObjectImplementation::isNearBank() {
-	Zone* zone = getZone();
 
-	if (zone != nullptr) {
-		SortedVector<TreeEntry* > closeObjects;
-		zone->getInRangeObjects(getPositionX(), getPositionZ(), getPositionY(), 15.f, &closeObjects, true, false);
+	SortedVector<ManagedReference<TreeEntry*> > closeObjects;
+	CloseObjectsVector* closeObjectsVector = (CloseObjectsVector*) getCloseObjects();
 
-		bool nearBank = false;
-
-		for (int i = 0; i < closeObjects.size(); ++i) {
-			SceneObject* sceneO = cast<SceneObject*>(closeObjects.get(i));
-
-			if (sceneO == nullptr)
-				continue;
-
-			if (sceneO->getGameObjectType() == SceneObjectType::BANK) {
-				nearBank = true;
-				break;
-			}
+	if (closeObjectsVector != nullptr) {
+		closeObjectsVector->safeCopyTo(closeObjects);
+	} else {
+		auto zone = getZone();
+		if (zone != nullptr) {
+			zone->getInRangeObjects(getWorldPositionX(), getWorldPositionZ(), getWorldPositionY(), 15.f, &closeObjects, true, true);
+		} else {
+			return false;
 		}
+	}
 
-		if (nearBank) {
-			return true;
-		}
+	uint64 parentID = getParentID();
+	float bankRangeSq = 15.f * 15.f;
+	Vector3 thisWorldPos = getWorldPosition();
+
+	for (int i = 0; i < closeObjects.size(); ++i) {
+
+		ManagedReference<SceneObject*> sceneO = cast<SceneObject*>(closeObjects.get(i).get());
+
+		if (sceneO == nullptr)
+			continue;
+
+		if (sceneO->getParentID() != parentID)
+			continue;
+
+		if (sceneO->getGameObjectType() != SceneObjectType::BANK)
+			continue;
+
+		if (closeObjectsVector != nullptr && thisWorldPos.squaredDistanceTo(sceneO->getWorldPosition()) > bankRangeSq)
+			continue;
+
+		return true;
+
 	}
 
 	return false;