swgemu/Core3

[added] loading of space spawn groups from lua [added] space spawn area spawning to use spawn groups assigned to spawn area
Change-Id: Icf365478d5bb2458b39c3f44ca4c0de00310ba1e

MMOCoreORB/src/server/zone/objects/region/space/SpaceSpawnAreaImplementation.cpp
@@ -11,6 +11,10 @@
 #include "server/zone/managers/ship/ShipManager.h"
 #include "server/zone/objects/ship/ai/ShipAiAgent.h"
 #include "server/zone/objects/region/space/SpaceSpawnAreaObserver.h"
+#include "server/zone/managers/ship/SpaceSpawn.h"
+#include "server/zone/managers/space/SpaceManager.h"
+#include "server/zone/objects/intangible/TheaterObject.h"
+#include "server/zone/managers/creature/DynamicSpawnObserver.h"
 
 // #define DEBUG_SPACE_SPAWNING
 
@@ -135,54 +139,125 @@ void SpaceSpawnAreaImplementation::tryToSpawn(ShipObject* playerShip) {
 		return;
 	}
 
+	auto zoneServer = getZoneServer();
+
+	if (zoneServer == nullptr) {
+		return;
+	}
+
+	auto spaceManager = zone->getSpaceManager();
+
+	if (spaceManager == nullptr) {
+		return;
+	}
+
 	// Update lastSpawn time
 	lastSpawn.updateToCurrentTime();
 
-	// Temporary TODO: Remove
-	Vector<String> tempShips = {"object/ship/freighterlight_tier1.iff", "object/ship/z95_tier1.iff", "object/ship/yt1300_tier1.iff", "object/ship/sorosuub_space_yacht.iff", "object/ship/merchant_cruiser_light_tier1.iff"};
-	String templateToSpawn = tempShips.get(System::random(tempShips.size() - 1));
+	int choice = System::random(totalWeighting - 1);
+	int counter = 0;
 
+	SpaceSpawn* finalSpawn = nullptr;
 
-	// Spawn ShipAiAgent
-	ManagedReference<ShipAiAgent*> shipAgent = ShipManager::instance()->createAiShip(templateToSpawn);
+	for (int i = 0; i < possibleSpawns.size(); i++) {
+		SpaceSpawn* spawn = possibleSpawns.get(i);
 
-	if (shipAgent == nullptr) {
-		error() << "Failed to spawn AI Ship - invalid ship agent template: " << templateToSpawn;
+		counter += spawn->getWeighting();
+
+		if (choice < counter) {
+			finalSpawn = spawn;
+			break;
+		}
+	}
+
+	if (finalSpawn == nullptr) {
+#ifdef DEBUG_SPACE_SPAWNING
+		info(true) << "tryToSpawn -- finalSpawn is a nullptr";
+#endif // DEBUG_SPACE_SPAWNING
 		return;
 	}
 
-	Vector3 position = getRandomPosition();
-
-	Locker lock(shipAgent);
-
-	shipAgent->initializePosition(position.getX(), position.getZ(), position.getY());
-
-	shipAgent->setHomeLocation(position.getX(), position.getZ(), position.getY(), Quaternion::IDENTITY);
-	shipAgent->initializeTransform(position, Quaternion::IDENTITY);
-
-	if (!zone->transferObject(shipAgent, -1, true)) {
-		shipAgent->destroyObjectFromWorld(true);
-		shipAgent->destroyObjectFromDatabase(true);
-
-		return;
-	}
-
-	// Set to despawn with no players in range
-	shipAgent->setDespawnOnNoPlayerInRange(true);
-
+	// Create observer for the area if it does not exist
 	if (spawnAreaObserver == nullptr) {
 		spawnAreaObserver = new SpaceSpawnAreaObserver(_this.getReferenceUnsafeStaticCast());
 		spawnAreaObserver->deploy();
 	}
 
-	// Register the observer
-	shipAgent->registerObserver(ObserverEventType::OBJECTREMOVEDFROMZONE, spawnAreaObserver);
+	// Get a random position within the spawn area
+	Vector3 position = getRandomPosition();
+
+	// Create the theater
+	Reference<TheaterObject*> theater = zoneServer->createObject(STRING_HASHCODE("object/intangible/theater/base_theater.iff"), 0).castTo<TheaterObject*>();
+
+	if (theater == nullptr) {
+		error() << "error creating intangible theater";
+		return;
+	}
+
+	Locker theaterLocker(theater);
+
+	theater->initializePosition(position.getX(), position.getZ(), position.getY());
+	theater->setDespawnOnNoPlayersInRange(true);
+	theater->registerObserver(ObserverEventType::OBJECTREMOVEDFROMZONE, spawnAreaObserver);
+
+	// TODO: Create SpaceSpawnObserver for this
+	ManagedReference<DynamicSpawnObserver*> dynamicObserver = new DynamicSpawnObserver();
+
+	if (dynamicObserver != nullptr) {
+		dynamicObserver->deploy();
+
+		dynamicObserver->setObserverType(ObserverType::LAIR);
+		dynamicObserver->setSize(512.f);
+
+		theater->registerObserver(ObserverEventType::CREATUREDESPAWNED, dynamicObserver);
+		theater->registerObserver(ObserverEventType::OBJECTREMOVEDFROMZONE, dynamicObserver);
+	}
+
+	zone->transferObject(theater, -1, true);
+
+	// Release the lock on the theater
+	theaterLocker.release();
+
+	// Get the total amount of ships to spawn
+	int numberToSpawn = finalSpawn->getNumberToSpawn();
+
+	uint32 capitalShipCRC = finalSpawn->getCapitalShipCRC();
+
+	// Spawn the Capital Ship
+	ManagedReference<SceneObject*> capitalShip = spaceManager->spaceDynamicSpawn(capitalShipCRC, zone, position, theater);
+
+	if (capitalShip != nullptr) {
+#ifdef DEBUG_SPACE_SPAWNING
+		info(true) << "captial ship is not nullptr";
+#endif // DEBUG_SPACE_SPAWNING
+	}
+
+	// First spawn the lead ship
+	const auto randomLeadShip = finalSpawn->getRandomLeadShip();
+
+	// Spawn lead ShipAiAgent
+	ManagedReference<SceneObject*> leadShip = spaceManager->spaceDynamicSpawn(randomLeadShip, zone, position, theater);
+
+	if (leadShip != nullptr) {
+		numberToSpawn--;
+	}
+
+	while (numberToSpawn > 0) {
+		// Get a random group ship
+		const auto randomGroupShip = finalSpawn->getRandomGroupShip();
+
+		// Spawn lead ShipAiAgent
+		ManagedReference<SceneObject*> groupShip = spaceManager->spaceDynamicSpawn(randomGroupShip, zone, position, theater);
+
+		// Decrease amount to spawn
+		numberToSpawn--;
+	}
 
 	// Increase Spawn Count
 	totalSpawnCount++;
 
 #ifdef DEBUG_SPACE_SPAWNING
-	info(true) << "A new ship has spawned!";
+	info(true) << "A new ship lair has spawned!";
 #endif // DEBUG_SPACE_SPAWNING
 }
 
@@ -198,12 +273,42 @@ Vector3 SpaceSpawnAreaImplementation::getRandomPosition() {
 	Vector3 position = areaShape->getRandomPosition(getWorldPosition(), 256.f, ZoneServer::SPACEOBJECTRANGE);
 
 #ifdef DEBUG_SPACE_SPAWNING
-	info(true) << getAreaName() << " -- getRandomPosition --  Location = " << getPositionX() << " , " << getPositionY();
+	info(true) << getAreaName() << " -- getRandomPosition --  Location = " << position.toString();
 #endif // DEBUG_SPACE_SPAWNING
 
 	return position;
 }
 
 void SpaceSpawnAreaImplementation::buildSpawnList(Vector<uint32>* groupCRCs) {
+	auto shipManager = ShipManager::instance();
 
+	if (shipManager == nullptr) {
+		return;
+	}
+
+#ifdef DEBUG_SPACE_SPAWNING
+	info(true) << "buildSpawnList from " << groupCRCs->size() << " total groups";
+#endif // DEBUG_SPACE_SPAWNING
+
+	for (int i = 0; i < groupCRCs->size(); i++) {
+		SpaceSpawnGroup* group = shipManager->getSpaceSpawnGroup(groupCRCs->get(i));
+
+		if (group == nullptr) {
+			continue;
+		}
+
+		const Vector<Reference<SpaceSpawn*> >& spawnList = group->getSpawnList();
+
+#ifdef DEBUG_SPACE_SPAWNING
+		info(true) << "Group has spawnList with a size of " << spawnList.size();
+#endif // DEBUG_SPACE_SPAWNING
+
+		for (int j = 0; j < spawnList.size(); j++) {
+			const auto& spawn = spawnList.get(j);
+
+			possibleSpawns.emplace(spawn);
+
+			totalWeighting += spawn->getWeighting();
+		}
+	}
 }

swgsremu/sentinelsrepublic-server

MMOCoreORB/src/server/zone/objects/region/space/SpaceSpawnAreaImplementation.cpp
@@ -11,6 +11,10 @@
 #include "server/zone/managers/ship/ShipManager.h"
 #include "server/zone/objects/ship/ai/ShipAiAgent.h"
 #include "server/zone/objects/region/space/SpaceSpawnAreaObserver.h"
+#include "server/zone/managers/ship/SpaceSpawn.h"
+#include "server/zone/managers/space/SpaceManager.h"
+#include "server/zone/objects/intangible/TheaterObject.h"
+#include "server/zone/managers/ship/SpaceSpawnObserver.h"
 
 // #define DEBUG_SPACE_SPAWNING
 
@@ -103,7 +107,7 @@ int SpaceSpawnAreaImplementation::notifyObserverEvent(unsigned int eventType, Ob
 
 	SceneObject* sceneO = dynamic_cast<SceneObject*>(observable);
 
-	if (sceneO == nullptr || !sceneO->isShipAiAgent()) {
+	if (sceneO == nullptr) {
 		return 1;
 	}
 
@@ -135,54 +139,132 @@ void SpaceSpawnAreaImplementation::tryToSpawn(ShipObject* playerShip) {
 		return;
 	}
 
+	auto zoneServer = getZoneServer();
+
+	if (zoneServer == nullptr) {
+		return;
+	}
+
+	auto spaceManager = zone->getSpaceManager();
+
+	if (spaceManager == nullptr) {
+		return;
+	}
+
 	// Update lastSpawn time
 	lastSpawn.updateToCurrentTime();
 
-	// Temporary TODO: Remove
-	Vector<String> tempShips = {"object/ship/freighterlight_tier1.iff", "object/ship/z95_tier1.iff", "object/ship/yt1300_tier1.iff", "object/ship/sorosuub_space_yacht.iff", "object/ship/merchant_cruiser_light_tier1.iff"};
-	String templateToSpawn = tempShips.get(System::random(tempShips.size() - 1));
+	int choice = System::random(totalWeighting - 1);
+	int counter = 0;
 
+	SpaceSpawn* finalSpawn = nullptr;
 
-	// Spawn ShipAiAgent
-	ManagedReference<ShipAiAgent*> shipAgent = ShipManager::instance()->createAiShip(templateToSpawn);
+	for (int i = 0; i < possibleSpawns.size(); i++) {
+		SpaceSpawn* spawn = possibleSpawns.get(i);
 
-	if (shipAgent == nullptr) {
-		error() << "Failed to spawn AI Ship - invalid ship agent template: " << templateToSpawn;
+		counter += spawn->getWeighting();
+
+		if (choice < counter) {
+			finalSpawn = spawn;
+			break;
+		}
+	}
+
+	if (finalSpawn == nullptr) {
+#ifdef DEBUG_SPACE_SPAWNING
+		info(true) << "tryToSpawn -- finalSpawn is a nullptr";
+#endif // DEBUG_SPACE_SPAWNING
 		return;
 	}
 
-	Vector3 position = getRandomPosition();
-
-	Locker lock(shipAgent);
-
-	shipAgent->initializePosition(position.getX(), position.getZ(), position.getY());
-
-	shipAgent->setHomeLocation(position.getX(), position.getZ(), position.getY(), Quaternion::IDENTITY);
-	shipAgent->initializeTransform(position, Quaternion::IDENTITY);
-
-	if (!zone->transferObject(shipAgent, -1, true)) {
-		shipAgent->destroyObjectFromWorld(true);
-		shipAgent->destroyObjectFromDatabase(true);
-
-		return;
-	}
-
-	// Set to despawn with no players in range
-	shipAgent->setDespawnOnNoPlayerInRange(true);
-
+	// Create observer for the area if it does not exist
 	if (spawnAreaObserver == nullptr) {
 		spawnAreaObserver = new SpaceSpawnAreaObserver(_this.getReferenceUnsafeStaticCast());
 		spawnAreaObserver->deploy();
 	}
 
-	// Register the observer
-	shipAgent->registerObserver(ObserverEventType::OBJECTREMOVEDFROMZONE, spawnAreaObserver);
+	// Get a random position within the spawn area
+	Vector3 position = getRandomPosition();
+
+	// Create the theater
+	Reference<TheaterObject*> theater = zoneServer->createObject(STRING_HASHCODE("object/intangible/theater/base_theater.iff"), 0).castTo<TheaterObject*>();
+	ManagedReference<SpaceSpawnObserver*> spaceObserver = new SpaceSpawnObserver();
+
+	if (theater == nullptr || spaceObserver == nullptr) {
+		error() << "error creating intangible theater or SpaceObserver";
+		return;
+	}
+
+	Locker theaterLocker(theater);
+
+	theater->initializePosition(position.getX(), position.getZ(), position.getY());
+	theater->setDespawnOnNoPlayersInRange(true);
+
+	// Register the observer for the SpaceSpawnArea so it can keep count of current spawns
+	theater->registerObserver(ObserverEventType::OBJECTREMOVEDFROMZONE, spawnAreaObserver);
+
+	// Setup the SpaceSpawnObserver
+	spaceObserver->deploy();
+
+	spaceObserver->setObserverType(ObserverType::SPACE_SPAWN);
+	spaceObserver->setSize(512.f);
+
+	theater->registerObserver(ObserverEventType::SHIPAGENTDESPAWNED, spaceObserver);
+	theater->registerObserver(ObserverEventType::OBJECTREMOVEDFROMZONE, spaceObserver);
+
+	zone->transferObject(theater, -1, true);
+
+	// Release the lock on the theater
+	theaterLocker.release();
+
+	// Get the total amount of ships to spawn
+	int numberToSpawn = finalSpawn->getNumberToSpawn();
+
+	uint32 capitalShipCRC = finalSpawn->getCapitalShipCRC();
+
+	// Spawn the Capital Ship
+	ManagedReference<SceneObject*> capitalShip = spaceManager->spaceDynamicSpawn(capitalShipCRC, zone, position, theater);
+
+	if (capitalShip != nullptr) {
+#ifdef DEBUG_SPACE_SPAWNING
+		info(true) << "captial ship is not nullptr";
+#endif // DEBUG_SPACE_SPAWNING
+
+		spaceObserver->setCapitalShip(capitalShip->asShipAiAgent());
+	}
+
+	// First spawn the lead ship
+	const auto randomLeadShip = finalSpawn->getRandomLeadShip();
+
+	// Spawn lead ShipAiAgent
+	ManagedReference<SceneObject*> leadShip = spaceManager->spaceDynamicSpawn(randomLeadShip, zone, position, theater);
+
+	if (leadShip != nullptr) {
+		spaceObserver->addSpawnedShip(leadShip->asShipAiAgent());
+
+		numberToSpawn--;
+	}
+
+	while (numberToSpawn > 0) {
+		// Get a random group ship
+		const auto randomGroupShip = finalSpawn->getRandomGroupShip();
+
+		// Spawn lead ShipAiAgent
+		ManagedReference<SceneObject*> groupShip = spaceManager->spaceDynamicSpawn(randomGroupShip, zone, position, theater);
+
+		if (groupShip != nullptr) {
+			spaceObserver->addSpawnedShip(groupShip->asShipAiAgent());
+		}
+
+		// Decrease amount to spawn
+		numberToSpawn--;
+	}
 
 	// Increase Spawn Count
 	totalSpawnCount++;
 
 #ifdef DEBUG_SPACE_SPAWNING
-	info(true) << "A new ship has spawned!";
+	info(true) << "A new ship lair has spawned!";
 #endif // DEBUG_SPACE_SPAWNING
 }
 
@@ -198,12 +280,42 @@ Vector3 SpaceSpawnAreaImplementation::getRandomPosition() {
 	Vector3 position = areaShape->getRandomPosition(getWorldPosition(), 256.f, ZoneServer::SPACEOBJECTRANGE);
 
 #ifdef DEBUG_SPACE_SPAWNING
-	info(true) << getAreaName() << " -- getRandomPosition --  Location = " << getPositionX() << " , " << getPositionY();
+	info(true) << getAreaName() << " -- getRandomPosition --  Location = " << position.toString();
 #endif // DEBUG_SPACE_SPAWNING
 
 	return position;
 }
 
 void SpaceSpawnAreaImplementation::buildSpawnList(Vector<uint32>* groupCRCs) {
+	auto shipManager = ShipManager::instance();
 
+	if (shipManager == nullptr) {
+		return;
+	}
+
+#ifdef DEBUG_SPACE_SPAWNING
+	info(true) << "buildSpawnList from " << groupCRCs->size() << " total groups";
+#endif // DEBUG_SPACE_SPAWNING
+
+	for (int i = 0; i < groupCRCs->size(); i++) {
+		SpaceSpawnGroup* group = shipManager->getSpaceSpawnGroup(groupCRCs->get(i));
+
+		if (group == nullptr) {
+			continue;
+		}
+
+		const Vector<Reference<SpaceSpawn*> >& spawnList = group->getSpawnList();
+
+#ifdef DEBUG_SPACE_SPAWNING
+		info(true) << "Group has spawnList with a size of " << spawnList.size();
+#endif // DEBUG_SPACE_SPAWNING
+
+		for (int j = 0; j < spawnList.size(); j++) {
+			const auto& spawn = spawnList.get(j);
+
+			possibleSpawns.emplace(spawn);
+
+			totalWeighting += spawn->getWeighting();
+		}
+	}
 }