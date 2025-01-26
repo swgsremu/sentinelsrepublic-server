swgemu/Core3

[added] starship component reverse engineering
Change-Id: Ia87a33cdf8458b612d6c52ba308295bf5078ad4a

MMOCoreORB/src/server/zone/objects/player/sui/callbacks/NameComponentSuiCallback.h
@@ -1,0 +1,86 @@
+/*
+ * NameComponentSuiCallback.h
+ *
+ *  Created on: Jan 25, 2025
+ *  	Author: Halyn
+ */
+
+#ifndef NAMECOMPONENTSUICALLBACK_H_
+#define NAMECOMPONENTSUICALLBACK_H_
+
+#include "server/zone/objects/player/sui/SuiCallback.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/ZoneProcessServer.h"
+#include "server/zone/managers/name/NameManager.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+
+class NameComponentSuiCallback : public SuiCallback {
+public:
+	NameComponentSuiCallback(ZoneServer* serv) : SuiCallback(serv) {
+	}
+
+	void run(CreatureObject* player, SuiBox* sui, uint32 eventIndex, Vector<UnicodeString>* args) {
+		if (player == nullptr)
+			return;
+
+		bool cancelPressed = (eventIndex == 1);
+
+		if (!sui->isInputBox() || cancelPressed || args->size() < 1)
+			return;
+
+		ManagedReference<SceneObject*> object = sui->getUsingObject().get();
+
+		if (object == nullptr || !object->isShipComponentObject())
+			return;
+
+		String componentName = args->get(0).toString();
+		bool emptyName = true;
+
+		if (!componentName.isEmpty()) {
+			emptyName = false;
+
+			ZoneProcessServer* zps = player->getZoneProcessServer();
+
+			if (zps == nullptr)
+				return;
+
+			NameManager* nameManager = zps->getNameManager();
+
+			if (nameManager == nullptr)
+				return;
+
+			// Player Entered a name, validate with nane manager
+			if (nameManager->checkNamingFilter(componentName) != NameManagerResult::ACCEPTED) {
+				player->sendSystemMessage("@reverse_engineering_tool:reserved");
+				//re-send SUI
+				auto ghost = player->getPlayerObject();
+
+				if (ghost == nullptr)
+					return;
+
+				ManagedReference<SuiInputBox*> inputBox = new SuiInputBox(player, SuiWindowType::OBJECT_NAME);
+
+				inputBox->setUsingObject(object);
+
+				inputBox->setPromptTitle("@reverse_engineering_tool:title2"); // Name New Component
+				inputBox->setPromptText("@reverse_engineering_tool:prompt2"); // That is a reserved name.
+
+				inputBox->setMaxInputSize(20);
+
+				inputBox->setCallback(new NameComponentSuiCallback(player->getZoneServer()));
+				inputBox->setForceCloseDistance(-1);
+
+				ghost->addSuiBox(inputBox);
+
+				player->sendMessage(inputBox->generateMessage());
+				return;
+			}
+
+			Locker componentLock(object, player);
+
+			object->setCustomObjectName(componentName, true);
+		}
+	}
+};
+
+#endif /* NAMESHIPSUICALLBACK_H_ */

MMOCoreORB/src/server/zone/objects/scene/SceneObjectType.h
@@ -221,6 +221,7 @@ public:
 	const static int ARMORUPGRADEKIT = 0x800B;
 	const static int RECYCLETOOL = 0x800C;
 	const static int ANTIDECAYKIT = 0x800D;
+	const static int COMPONENTANALYSISTOOL = 0x800E;
 	// End Not in client *****************
 
 	const static int VEHICLE = 0x10000;

swgsremu/sentinelsrepublic-server

MMOCoreORB/src/server/zone/objects/player/sui/callbacks/NameComponentSuiCallback.h
@@ -1,0 +1,85 @@
+/*
+ * NameComponentSuiCallback.h
+ *
+ *  Created on: Jan 25, 2025
+ *  	Author: Halyn
+ */
+
+#ifndef NAMECOMPONENTSUICALLBACK_H_
+#define NAMECOMPONENTSUICALLBACK_H_
+
+#include "server/zone/objects/player/sui/SuiCallback.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/ZoneProcessServer.h"
+#include "server/zone/managers/name/NameManager.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+
+class NameComponentSuiCallback : public SuiCallback {
+public:
+	NameComponentSuiCallback(ZoneServer* serv) : SuiCallback(serv) {
+	}
+
+	void run(CreatureObject* player, SuiBox* sui, uint32 eventIndex, Vector<UnicodeString>* args) {
+		if (player == nullptr || sui == nullptr)
+			return;
+
+		bool cancelPressed = (eventIndex == 1);
+
+		if (!sui->isInputBox() || cancelPressed || args->size() < 1)
+			return;
+
+		ManagedReference<SceneObject*> object = sui->getUsingObject().get();
+
+		if (object == nullptr || !object->isShipComponentObject())
+			return;
+
+		String componentName = args->get(0).toString();
+
+		if (componentName.isEmpty())
+			return;
+
+		ZoneProcessServer* zoneProcess = player->getZoneProcessServer();
+
+		if (zoneProcess == nullptr)
+			return;
+
+		NameManager* nameManager = zoneProcess->getNameManager();
+
+		if (nameManager == nullptr)
+			return;
+
+		// Player Entered a name, validate with nane manager
+		if (nameManager->checkNamingFilter(componentName) != NameManagerResult::ACCEPTED) {
+			player->sendSystemMessage("@reverse_engineering_tool:reserved");
+
+			//re-send SUI
+			auto ghost = player->getPlayerObject();
+
+			if (ghost == nullptr)
+				return;
+
+			ManagedReference<SuiInputBox*> inputBox = new SuiInputBox(player, SuiWindowType::OBJECT_NAME);
+
+			inputBox->setUsingObject(object);
+
+			inputBox->setPromptTitle("@reverse_engineering_tool:title2"); // Name New Component
+			inputBox->setPromptText("@reverse_engineering_tool:prompt2"); // That is a reserved name.
+
+			inputBox->setMaxInputSize(20);
+
+			inputBox->setCallback(new NameComponentSuiCallback(player->getZoneServer()));
+			inputBox->setForceCloseDistance(-1);
+
+			ghost->addSuiBox(inputBox);
+
+			player->sendMessage(inputBox->generateMessage());
+			return;
+		}
+
+		Locker componentLock(object, player);
+
+		object->setCustomObjectName(componentName, true);
+	}
+};
+
+#endif /* NAMECOMPONENTSUICALLBACK_H_ */

MMOCoreORB/src/server/zone/objects/scene/SceneObjectType.h
@@ -170,6 +170,7 @@ public:
 	const static int LIGHTOBJECT = 0x203D;
 	const static int SPACESPAWNER = 0x203E;
 	const static int CREDITCHIP = 0x203F;
+	const static int COMPONENTANALYSISTOOL = 0x2040;
 	/// End Not in client **************
 
 	const static int TERMINAL = 0x4000;