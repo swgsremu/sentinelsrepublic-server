swgemu/Core3

[added] parent space quest screenplay [added] space quest destroy, patrol, escort and surprise attack generic quest logic [fixed] space faction checking for attackable, aggressiveness and enemy flagging [added] additional space helper quest and player management functions
Change-Id: I1df205b86d987f898face597e251cefd585b4ffa

MMOCoreORB/bin/scripts/screenplays/events/conversations/life_day_kkatamk_conv_handler.lua
@@ -9,7 +9,7 @@ function lifeDayKkatamkConvoHandler:getInitialScreen(pPlayer, pNpc, pConvTemplat
 		return convoTemplate:getScreen("return_complete")
 	elseif readData(CreatureObject(pPlayer):getObjectID() .. ":lifeDayState") == 1 then
 		return convoTemplate:getScreen("return_incomplete")
-	elseif CreatureObject(pPlayer):getSpecies() == 4 then
+	elseif CreatureObject(pPlayer):getSpecies() == SPECIES_WOOKIE then
 		return convoTemplate:getScreen("greetings_wookiee")
 	else
 		return convoTemplate:getScreen("greetings")

MMOCoreORB/bin/scripts/screenplays/events/conversations/life_day_oraalarri_conv_handler.lua
@@ -32,7 +32,7 @@ function lifeDayOraalarriConvoHandler:runScreenHandlers(pConvTemplate, pPlayer, 
 	if screenID == "speak_to_others" then
 		writeData(SceneObject(pPlayer):getObjectID() .. ":lifeDayState", 2)
 	elseif screenID == "have_gifts" then
-		if CreatureObject(pPlayer):getSpecies() == 4 then
+		if CreatureObject(pPlayer):getSpecies() == SPECIES_WOOKIE then
 			clonedConversation:addOption("@conversation/lifeday04b:s_afabf3ac", "gift_wookiee") -- What sort of gifts?
 		else
 			clonedConversation:addOption("@conversation/lifeday04b:s_afabf3ac", "gift_non_wookiee") -- What sort of gifts?

MMOCoreORB/src/server/zone/managers/director/DirectorManager.cpp
@@ -635,6 +635,18 @@ void DirectorManager::initializeLuaEngine(Lua* luaEngine) {
 	luaEngine->setGlobalInt("CROUCHED", CreaturePosture::CROUCHED);
 	luaEngine->setGlobalInt("LYINGDOWN", CreaturePosture::LYINGDOWN);
 
+	// Player Species
+	luaEngine->setGlobalInt("SPECIES_HUMAN", CreatureObject::HUMAN);
+	luaEngine->setGlobalInt("SPECIES_RODIAN", CreatureObject::RODIAN);
+	luaEngine->setGlobalInt("SPECIES_TRANDOSHAN", CreatureObject::TRANDOSHAN);
+	luaEngine->setGlobalInt("SPECIES_MONCAL", CreatureObject::MONCAL);
+	luaEngine->setGlobalInt("SPECIES_WOOKIE", CreatureObject::WOOKIE);
+	luaEngine->setGlobalInt("SPECIES_BOTHAN", CreatureObject::BOTHAN);
+	luaEngine->setGlobalInt("SPECIES_TWILEK", CreatureObject::TWILEK);
+	luaEngine->setGlobalInt("SPECIES_ZABRAK", CreatureObject::ZABRAK);
+	luaEngine->setGlobalInt("SPECIES_ITHORIAN", CreatureObject::ITHORIAN);
+	luaEngine->setGlobalInt("SPECIES_SULLUSTAN", CreatureObject::SULLUSTAN);
+
 	//Player Arrangements
 	luaEngine->setGlobalLong("RIDER", PlayerArrangement::RIDER);
 	luaEngine->setGlobalLong("SHIP_PILOT", PlayerArrangement::SHIP_PILOT);

swgsremu/sentinelsrepublic-server

MMOCoreORB/bin/scripts/screenplays/events/conversations/life_day_kkatamk_conv_handler.lua
@@ -9,7 +9,7 @@ function lifeDayKkatamkConvoHandler:getInitialScreen(pPlayer, pNpc, pConvTemplat
 		return convoTemplate:getScreen("return_complete")
 	elseif readData(CreatureObject(pPlayer):getObjectID() .. ":lifeDayState") == 1 then
 		return convoTemplate:getScreen("return_incomplete")
-	elseif CreatureObject(pPlayer):getSpecies() == 4 then
+	elseif CreatureObject(pPlayer):getSpecies() == SPECIES_WOOKIEE then
 		return convoTemplate:getScreen("greetings_wookiee")
 	else
 		return convoTemplate:getScreen("greetings")

MMOCoreORB/bin/scripts/screenplays/events/conversations/life_day_oraalarri_conv_handler.lua
@@ -32,7 +32,7 @@ function lifeDayOraalarriConvoHandler:runScreenHandlers(pConvTemplate, pPlayer, 
 	if screenID == "speak_to_others" then
 		writeData(SceneObject(pPlayer):getObjectID() .. ":lifeDayState", 2)
 	elseif screenID == "have_gifts" then
-		if CreatureObject(pPlayer):getSpecies() == 4 then
+		if CreatureObject(pPlayer):getSpecies() == SPECIES_WOOKIEE then
 			clonedConversation:addOption("@conversation/lifeday04b:s_afabf3ac", "gift_wookiee") -- What sort of gifts?
 		else
 			clonedConversation:addOption("@conversation/lifeday04b:s_afabf3ac", "gift_non_wookiee") -- What sort of gifts?

MMOCoreORB/src/server/zone/managers/director/DirectorManager.cpp
@@ -635,6 +635,18 @@ void DirectorManager::initializeLuaEngine(Lua* luaEngine) {
 	luaEngine->setGlobalInt("CROUCHED", CreaturePosture::CROUCHED);
 	luaEngine->setGlobalInt("LYINGDOWN", CreaturePosture::LYINGDOWN);
 
+	// Player Species
+	luaEngine->setGlobalInt("SPECIES_HUMAN", CreatureObject::HUMAN);
+	luaEngine->setGlobalInt("SPECIES_RODIAN", CreatureObject::RODIAN);
+	luaEngine->setGlobalInt("SPECIES_TRANDOSHAN", CreatureObject::TRANDOSHAN);
+	luaEngine->setGlobalInt("SPECIES_MONCAL", CreatureObject::MONCAL);
+	luaEngine->setGlobalInt("SPECIES_WOOKIEE", CreatureObject::WOOKIEE);
+	luaEngine->setGlobalInt("SPECIES_BOTHAN", CreatureObject::BOTHAN);
+	luaEngine->setGlobalInt("SPECIES_TWILEK", CreatureObject::TWILEK);
+	luaEngine->setGlobalInt("SPECIES_ZABRAK", CreatureObject::ZABRAK);
+	luaEngine->setGlobalInt("SPECIES_ITHORIAN", CreatureObject::ITHORIAN);
+	luaEngine->setGlobalInt("SPECIES_SULLUSTAN", CreatureObject::SULLUSTAN);
+
 	//Player Arrangements
 	luaEngine->setGlobalLong("RIDER", PlayerArrangement::RIDER);
 	luaEngine->setGlobalLong("SHIP_PILOT", PlayerArrangement::SHIP_PILOT);