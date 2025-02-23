d09f7d75bf2080feecc17381ff717854a0b49c25
Renagade <thomasholter@outlook.com>
2025-02-23 21:49:19 +0100

    [SR: Modified] SRConfigManager to correctly read config file

--- MMOCoreORB/src/conf/custom/SRConfigManager.cpp
+++ MMOCoreORB/src/conf/custom/SRConfigManager.cpp
@@ -1,35 +1,46 @@
 #include "SRConfigManager.h"
 
 SRConfigManager::SRConfigManager() : ConfigManager() {
-    loadConfigData(); // Load base config
-    loadCustomConfig(); // Load custom config
+	loadConfigData();	// Load base config
+	loadCustomConfig(); // Load custom config
 }
 
 bool SRConfigManager::getStructurePackupEnabled() {
-    return getBool("Core3.structurePackupEnabled", false);
+	return getBool("SRConfig.structurePackupEnabled", false);
 }
 
 bool SRConfigManager::getInactiveStructurePackupEnabled() {
-    return getBool("Core3.inactiveStructurePackupEnabled", false);
+	return getBool("SRConfig.inactiveStructurePackupEnabled", false);
 }
 
 int SRConfigManager::getInactiveStructurePackupDays() {
-    return getInt("Core3.inactiveStructurePackupDays", 365);
+	return getInt("SRConfig.inactiveStructurePackupDays", 365);
 }
 
 void SRConfigManager::loadCustomConfig() {
-    Lua* lua = new Lua();
-    lua->init();
+	Lua* lua = new Lua();
+	lua->init();
 
-    if (!lua->runFile("custom/conf/config.lua")) {
-        error("Failed to load custom/conf/config.lua");
-        delete lua;
-        return;
-    }
+	if (!lua->runFile("conf/custom/config.lua")) {
+		info() << "Failed to load conf/custom/config.lua";
+		delete lua;
+		return;
+	}
 
-    setBool("Core3.structurePackupEnabled", lua->getGlobalInt("structurePackupEnabled"));
-    setBool("Core3.inactiveStructurePackupEnabled", lua->getGlobalInt("inactiveStructurePackupEnabled"));
-    setInt("Core3.inactiveStructurePackupDays", lua->getGlobalInt("inactiveStructurePackupDays"));
+	// Append custom settings from SRConfig table
+    LuaObject srConfig = lua->getGlobalObject("SRConfig");
+    	if (srConfig.isValidTable()) {
+		String packupEnabled = srConfig.getStringField("structurePackupEnabled");
+		setBool("SRConfig.structurePackupEnabled", packupEnabled == "true" ? true : false);
 
-    delete lua;
+		String inactivePackupEnabled = srConfig.getStringField("inactiveStructurePackupEnabled");
+		setBool("SRConfig.inactiveStructurePackupEnabled", inactivePackupEnabled == "true" ? true : false);
+
+		setInt("SRConfig.inactiveStructurePackupDays", srConfig.getIntField("inactiveStructurePackupDays"));
+	} else {
+		error() << "SRConfig table not found in conf/custom/config.lua";
+	}
+	srConfig.pop();
+
+	delete lua;
 }
