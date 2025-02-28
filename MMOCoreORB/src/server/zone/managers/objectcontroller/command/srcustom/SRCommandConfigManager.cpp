046562d69c3c725443c8e24cf75cc1059426a782
Renagade <thomasholter@outlook.com>
2025-02-28 20:40:35 +0100

    [SR: Added] Add SRCommandConfigManager to register custom SR commands

---
+++ MMOCoreORB/src/server/zone/managers/objectcontroller/command/srcustom/SRCommandConfigManager.cpp
@@ -0,0 +1,32 @@
+/************************************************************************
+*  @author Sentinels Republic
+ *  @copyright Copyright 2024 Sentinels Republic.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+
+#include "../CommandConfigManager.h"
+
+using namespace server::zone::managers::objectcontroller::command;
+
+/**
+ * @brief Registers custom SR commands.
+ *
+ * This method is intended to register any custom commands specific
+ * to the Sentinels Republic. It should be implemented to add the
+ * custom commands to the command processing system.
+ */
+void CommandConfigManager::registerSrCommands() {
+}
