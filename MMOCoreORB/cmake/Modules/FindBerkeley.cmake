cdd0dcb0f68d7a238e24f9045bf0cee5e02b5b9f
Renagade <thomasholter@outlook.com>
2025-02-28 18:54:16 +0100

    [SR: Modified] Update FindBerkeley.cmake to include additional include and library paths

--- MMOCoreORB/cmake/Modules/FindBerkeley.cmake
+++ MMOCoreORB/cmake/Modules/FindBerkeley.cmake
@@ -32,13 +32,14 @@ FIND_PATH(DB_INCLUDE_DIR db.h
         /usr/local/BerkeleyDB.5.3/include
 	/usr/local/include
 	/usr/include
+	/usr/include/db5.3
         NO_DEFAULT_PATH
         )
 ENDIF ()
 
 FIND_LIBRARY(DB_LIBRARIES
         NAMES db-5.3 db
-        PATHS /opt/local/lib/db53 /usr/local/BerkeleyDB.5.3/lib /usr/lib/x86_64-linux-gnu/ /usr/local/lib
+        PATHS /opt/local/lib/db53 /usr/local/BerkeleyDB.5.3/lib /usr/lib/x86_64-linux-gnu/ /usr/local/lib /usr/lib
         )
 
 IF (DB_INCLUDE_DIR AND DB_LIBRARIES)
