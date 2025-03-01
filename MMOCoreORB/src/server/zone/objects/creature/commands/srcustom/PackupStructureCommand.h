266fc22e54f6ec86952af3ccc5ee50c46fbb7754
Renagade <thomasholter@outlook.com>
2025-03-01 02:48:48 +0100

    [SR: Modified] Add constants for error messages in PackupStructureCommand

--- MMOCoreORB/src/server/zone/objects/creature/commands/srcustom/PackupStructureCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/srcustom/PackupStructureCommand.h
@@ -10,6 +10,10 @@ public:
 	PackupStructureCommand(const String& name, ZoneProcessServer* server);
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const override;
+
+private:
+	static const std::string PACKUP_MUST_BE_OWNER;
+	static const std::string PACKUP_NOT_ELIGIBLE;
 };
 
 #endif // PACKUPSTRUCTURECOMMAND_H_
