7c961ad28e8d7bf12222a9fd48057937f0ba1af9
Renagade <thomasholter@outlook.com>
2025-03-01 02:29:11 +0100

    [SR: Modified] Add documentation comments for PackupStructureCommand methods

--- MMOCoreORB/src/server/zone/objects/creature/commands/srcustom/PackupStructureCommand.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/commands/srcustom/PackupStructureCommand.cpp
@@ -2,15 +2,29 @@
 #include "PackupStructureCommand.h"
 
 
+/**
+ * @brief Constructor for the PackupStructureCommand class.
+ *
+ * @param name The name of the command.
+ * @param server Pointer to the ZoneProcessServer instance.
+ */
 PackupStructureCommand::PackupStructureCommand(const String& name, ZoneProcessServer* server)
-    : QueueCommand(name, server) {
+	: QueueCommand(name, server) {
 }
 
+/**
+ * @brief Executes the PackupStructureCommand.
+ *
+ * @param creature Pointer to the CreatureObject executing the command.
+ * @param target The target object ID.
+ * @param arguments Additional arguments for the command.
+ * @return int Status code indicating success or failure.
+ */
 int PackupStructureCommand::doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-    if (creature == nullptr) {
-        return GENERALERROR;
-    }
+	if (creature == nullptr) {
+		return GENERALERROR;
+	}
 
-    creature->sendSystemMessage("PackupStructureCommand executed! (Placeholder logic)");
-    return SUCCESS;
+	creature->sendSystemMessage("PackupStructureCommand executed! (Placeholder logic)");
+	return SUCCESS;
 }
