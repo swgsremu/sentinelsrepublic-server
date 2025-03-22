2f35db899221ac8981220d54151ffef2071be6e5
Matthew Ryan Todd <RokanThrex@gmail.com>
2025-03-22 14:36:21 -0500

    Update SR2HairValidator.h
    Working to get this file set up correct.

--- MMOCoreORB/src/server/zone/managers/player/creation/SR2HairValidator.h
+++ MMOCoreORB/src/server/zone/managers/player/creation/SR2HairValidator.h
@@ -1,9 +1,13 @@
 #ifndef SR2HAIRVALIDATOR_H
 #define SR2HAIRVALIDATOR_H
 
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/templates/CreatureTemplate.h"
+#include "server/zone/objects/appearance/HairAssetData.h"
+
 class SR2HairValidator {
 public:
-    static bool validate() {
+    static bool validate(HairAssetData* hairAssetData, CreatureObject* creature, const String& hairTemplate) {
         return true;
     }
 };
