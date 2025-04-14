300fc8cea271bdb8b852bc90008006552a4e5c49
Thomas Holter <thomasholter@outlook.com>
2025-04-14 17:26:41 +0200

    Update crafting timer configuration in config.lua

--- MMOCoreORB/bin/conf/custom/config.lua
+++ MMOCoreORB/bin/conf/custom/config.lua
@@ -1,5 +1,5 @@
 SRConfig = {
     factoryTimerMultiplier = 2,
-    craftingTimerInterval = 5,  -- Time in seconds to decrement from main timer
-    craftingTimerIncrement = 5000  -- Time in milliseconds to increment counter
+    craftingTimerInterval = 1,  -- Time in seconds to decrement from main timer
+    craftingTimerIncrement = 1000  -- Time in milliseconds to increment counter
 }
