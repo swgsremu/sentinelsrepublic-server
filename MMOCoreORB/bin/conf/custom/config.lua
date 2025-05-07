ba27ada93f1ab4b4ce0e33b7390ae19f5d0bbd18
Littleaton <55291686+Littleaton@users.noreply.github.com>
2025-05-07 14:23:01 -0600

    SR-104 Crafting Timer

--- MMOCoreORB/bin/conf/custom/config.lua
+++ MMOCoreORB/bin/conf/custom/config.lua
@@ -1,5 +1,5 @@
 SRConfig = {
     factoryTimerMultiplier = 2,
     craftingTimerInterval = 1,  -- Time in seconds to decrement from main timer
-    craftingTimerIncrement = 1000  -- Time in milliseconds to increment counter
+    craftingTimerIncrement = 1  -- Time in milliseconds to increment counter
 }
