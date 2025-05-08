194ceeb05e39d5a8e7843ffcc8e7d586760ce586
Littleaton <55291686+Littleaton@users.noreply.github.com>
2025-05-08 14:48:27 -0600

    SR-104 - Adjusted timers (#87)

--- MMOCoreORB/bin/conf/custom/config.lua
+++ MMOCoreORB/bin/conf/custom/config.lua
@@ -2,5 +2,5 @@ SRConfig = {
     -- factoryTimerMultiplier = 2,
     factoryTimerMultiplier = 1, -- Changed for crafting testing.
     craftingTimerInterval = 1,  -- Time in seconds to decrement from main timer
-    craftingTimerIncrement = 1  -- Time in milliseconds to increment counter
+    craftingTimerIncrement = 100  -- Time in milliseconds to increment counter
 }
