251aad0f19d7b47ccd49b2b19b1a1d1099fd4018
swgemu <swgemu@vbox.powerhub>
2025-05-29 12:03:51 +0100

    sr coin and fixed insrument errors

--- MMOCoreORB/bin/scripts/loot/serverobjects.lua
+++ MMOCoreORB/bin/scripts/loot/serverobjects.lua
@@ -1,5 +1,6 @@
 includeFile("items.lua")
 includeFile("groups.lua")
-
+includeFile("../custom_scripts/loot/items.lua")
+includeFile("../custom_scripts/loot/groups.lua")
 
 includeFile("../custom_scripts/loot/serverobjects.lua")
