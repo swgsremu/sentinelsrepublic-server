c99e9c290ac3d7b5f178f564a39658a7a1139f62
hakry <hakry@swgemu.com>
2025-06-02 11:09:38 -0400

    [added] spawnerID creature functions
    Change-Id: I1a6ae48062601ab293793b3a4a08b0978d3f04b2

--- MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
@@ -103,6 +103,7 @@ void CreatureObjectImplementation::initializeTransientMembers() {
 	groupInviteCounter = 0;
 	currentWeather = 0;
 	currentWind = 0;
+	spawnerID = 0;
 
 	if (creditObject == nullptr) {
 		creditObject = getCreditObject();
