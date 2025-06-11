4887ef2a13d2d228599268ddcda9e839ead7494d
Littleaton <55291686+Littleaton@users.noreply.github.com>
2025-06-11 11:50:39 -0600

    Fixed SR-170 (#156)
    * Delete MMOCoreORB/compile_commands.json:Zone.Identifier
    
    breaks checkout.
    
    Signed-off-by: Littleaton <55291686+Littleaton@users.noreply.github.com>
    
    * Fixed SR-170
    
    Can now buff anywhere if you have a medical module droid out.
    Fixed the buff calculations based on highest private_medical_rating value.
    Removed the invuln/invis shrinking...
    
    ---------
    
    Signed-off-by: Littleaton <55291686+Littleaton@users.noreply.github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- MMOCoreORB/src/server/zone/objects/tangible/pharmaceutical/WoundPackImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/pharmaceutical/WoundPackImplementation.cpp
@@ -9,41 +9,35 @@ bool WoundPackImplementation::isDroidReconstructionKit() {
 	return getObjectNameStringIdName().contains("droid_wound_kit");
 }
 uint32 WoundPackImplementation::calculatePower(CreatureObject* healer, CreatureObject* patient, bool applyBattleFatigue) {
-	float power = getEffectiveness();
+    float power = getEffectiveness();
 
-	if (applyBattleFatigue)
-		power *= patient->calculateBFRatio();
+    if (applyBattleFatigue)
+        power *= patient->calculateBFRatio();
 
-	if (isDroidReconstructionKit()) {
-		return power;
-	}
+    if (isDroidReconstructionKit()) {
+        return power;
+    }
 
-	int mod = healer->getSkillModOfType("private_medical_rating", SkillModManager::CITY);
+    int droidBuff = healer->getSkillModOfType("private_medical_rating", SkillModManager::DROID);
+    int structureBuff = healer->getSkillModOfType("private_medical_rating", SkillModManager::STRUCTURE);
+    int cityBuff = healer->getSkillModOfType("private_medical_rating", SkillModManager::CITY);
+    // Use the higher of droid or structure
+    int mod = cityBuff + std::max(droidBuff, structureBuff);
 
-	int droidBuff = healer->getSkillModOfType("private_medical_rating", SkillModManager::DROID);
-	int bldBuff = healer->getSkillModOfType("private_medical_rating", SkillModManager::STRUCTURE);
+    int factionPerk = healer->getSkillMod("private_faction_medical_rating");
 
-	// Mantis #8884 - Building buff ratings should override a droid buff rating
-	if (bldBuff > 0) {
-		mod += bldBuff;
-	} else {
-		mod += droidBuff;
-	}
+    ManagedReference<BuildingObject*> building = cast<BuildingObject*>(healer->getRootParent());
 
-	int factionPerk = healer->getSkillMod("private_faction_medical_rating");
+    if (building != nullptr && factionPerk > 0 && building->isPlayerRegisteredWithin(healer->getObjectID())) {
+        unsigned int buildingFaction = building->getFaction();
+        unsigned int healerFaction = healer->getFaction();
 
-	ManagedReference<BuildingObject*> building = cast<BuildingObject*>(healer->getRootParent());
+        if (healerFaction != 0 && healerFaction == buildingFaction && healer->getFactionStatus() == FactionStatus::OVERT) {
+            mod += factionPerk;
+        }
+    }
 
-	if (building != nullptr && factionPerk > 0 && building->isPlayerRegisteredWithin(healer->getObjectID())) {
-		unsigned int buildingFaction = building->getFaction();
-		unsigned int healerFaction = healer->getFaction();
-
-		if (healerFaction != 0 && healerFaction == buildingFaction && healer->getFactionStatus() == FactionStatus::OVERT) {
-			mod += factionPerk;
-		}
-	}
-
-	float modEnvironment = 1 + ((float) mod / 100.0f);
-	float modSkill = (float) healer->getSkillMod("healing_wound_treatment");
-	return (power * modEnvironment * (100.0f + modSkill) / 100.0f);
+    float modEnvironment = 1 + ((float) mod / 100.0f);
+    float modSkill = (float) healer->getSkillMod("healing_wound_treatment");
+    return (power * modEnvironment * (100.0f + modSkill) / 100.0f);
 }
