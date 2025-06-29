fee04346016f6f73c63e21271ab9f4ede47aa68f
TheGUI45 <deadpoolrappelz@gmail.com>
2025-06-29 08:50:48 -0400

    Fix Janta loot system: improve randomization, level scaling, and remove resource drops (#210)

--- MMOCoreORB/bin/scripts/loot/groups/npc/dantooine/janta_tribe_tier_1.lua
+++ MMOCoreORB/bin/scripts/loot/groups/npc/dantooine/janta_tribe_tier_1.lua
@@ -8,8 +8,6 @@ janta_tribe_tier_1 = {
 		{groupTemplate = "janta_common", weight = 1400000},
 		{groupTemplate = "junk", weight = 4900000},
 		{groupTemplate = "loot_kit_parts", weight = 2700000},
-		{groupTemplate = "resource_creature", weight = 750000},
-		{groupTemplate = "resource_water", weight = 250000},
 	}
 }
 
--- MMOCoreORB/bin/scripts/loot/groups/npc/dantooine/janta_tribe_tier_2.lua
+++ MMOCoreORB/bin/scripts/loot/groups/npc/dantooine/janta_tribe_tier_2.lua
@@ -8,8 +8,6 @@ janta_tribe_tier_2 = {
 		{groupTemplate = "janta_common", weight = 1400000},
 		{groupTemplate = "junk", weight = 5100000},
 		{groupTemplate = "loot_kit_parts", weight = 2700000},
-		{groupTemplate = "resource_creature", weight = 600000},
-		{groupTemplate = "resource_water", weight = 200000},
 	}
 }
 
