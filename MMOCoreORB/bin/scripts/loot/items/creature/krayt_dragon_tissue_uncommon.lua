7919d1919a63af36f1d69de5aa0557042958e28b
Idewu <alancode@gmail.com>
2025-06-30 11:13:45 -0700

    krayt loot second pass, tissues will now roll random white, and stablized tapes across all to increase based on CL of krayt

--- MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_uncommon.lua
+++ MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_uncommon.lua
@@ -6,9 +6,9 @@ krayt_dragon_tissue_uncommon = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/weapon/blaster_power_handler_enhancement_krayt.iff",
 	craftingValues = {
-		{"maxdamage",40,70,0},
-		{"attackspeed",-0.3,-1.2,2},
-		{"useCount",2,13,0},
+		{"maxdamage",40,70,0, false, 1},
+		{"attackspeed",-0.3,-1.2,2, false, 1},
+		{"useCount",2,13,0, false, 1},
 	},
 	customizationStringNames = {},
 	customizationValues = {}
