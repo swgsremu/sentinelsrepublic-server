d563f24c5a9ecd9fa5d377f99de1eaf4e4f86f04
Idewu <alancode@gmail.com>
2025-06-30 18:14:52 -0700

    removing extra commas

--- MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_common.lua
+++ MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_common.lua
@@ -6,7 +6,7 @@ krayt_dragon_tissue_common = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/weapon/blaster_power_handler_enhancement_krayt.iff",
 	craftingValues = {
-		{"maxdamage",30,60,0,},
+		{"maxdamage",30,60,0},
 		{"attackspeed",-0.3,-0.8,2},
 		{"useCount",2,13,0},
 	},
--- MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_rare.lua
+++ MMOCoreORB/bin/scripts/loot/items/creature/krayt_dragon_tissue_rare.lua
@@ -6,7 +6,7 @@ krayt_dragon_tissue_rare = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/weapon/blaster_power_handler_enhancement_krayt.iff",
 	craftingValues = {
-		{"maxdamage",80,165,0,},
+		{"maxdamage",80,165,0},
 		{"attackspeed",-0.3,-1.0, 2},
 		{"useCount",2,13,0,},
 	},
