a38f3496849d71f208ca4132f5280434f09cab64
DesporoWace <26713330+DesporoWace@users.noreply.github.com>
2025-10-24 18:51:12 +0100

    Updated RIS & Mandalorian armors to have some Lightsaber resists (#506)
    Updated RIS & Mandalorian armors to have some Lightsaber resists

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian/armor_mandalorian_leggings.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian/armor_mandalorian_leggings.lua
@@ -112,7 +112,7 @@ object_tangible_wearables_armor_mandalorian_armor_mandalorian_leggings = object_
 	heat = 58,
 	cold = 0,
 	acid = 58,
-	lightSaber = 0
+	lightSaber = 20
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_armor_mandalorian_armor_mandalorian_leggings, "object/tangible/wearables/armor/mandalorian/armor_mandalorian_leggings.iff")
