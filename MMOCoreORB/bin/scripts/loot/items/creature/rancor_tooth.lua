b7dc908273f93d8a9dc6a8b8f6373e56edb7c640
Asifab <54614510+Asifab19@users.noreply.github.com>
2025-10-26 16:42:44 -0400

    Update rancor_tooth.lua #6 (#514)

--- MMOCoreORB/bin/scripts/loot/items/creature/rancor_tooth.lua
+++ MMOCoreORB/bin/scripts/loot/items/creature/rancor_tooth.lua
@@ -6,7 +6,7 @@ rancor_tooth = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/weapon/rancor_tooth.iff",
 	craftingValues = {
-		{"mindamage",20,60,0},
+		{"mindamage",35,90,0},
 		{"maxdamage",50,135,0},
 		{"attackspeed",1.0,-0.5,2},
 		{"woundchance",10,20,0},
