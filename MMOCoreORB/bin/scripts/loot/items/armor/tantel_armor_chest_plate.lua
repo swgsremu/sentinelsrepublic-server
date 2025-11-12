2b19302a05e4779e339f8e3c01c286cc47c3dd02
Asifab <54614510+Asifab19@users.noreply.github.com>
2025-11-12 15:38:40 -0500

    Tantel armor cold resistance 2nd pass (#535)
    * Update tantel_armor_boots.lua
    
    second attempt to put cold protection on crafted tantel armor. 
    
    Signed-off-by: Asifab <54614510+Asifab19@users.noreply.github.com>
    
    * Update tantel_armor_chest_plate.lua
    
    second attempt to put cold resistance in special protection.
    
    Signed-off-by: Asifab <54614510+Asifab19@users.noreply.github.com>
    
    * Update tantel_armor_helmet.lua
    
    second pass at trying to add cold protection to special resistance. 
    
    Signed-off-by: Asifab <54614510+Asifab19@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Asifab <54614510+Asifab19@users.noreply.github.com>

--- MMOCoreORB/bin/scripts/loot/items/armor/tantel_armor_chest_plate.lua
+++ MMOCoreORB/bin/scripts/loot/items/armor/tantel_armor_chest_plate.lua
@@ -6,6 +6,7 @@ tantel_armor_chest_plate = {
 	craftingValues = {
 		{"armor_rating",1,1,0},
 		{"energyeffectiveness",10,30,10},
+		{"coldeffectiveness",10,30,10}, -- Adding cold resistance to special protection
 		{"armor_effectiveness",4,19,10},
 		{"armor_integrity",11250, 18750,0},
 		{"armor_health_encumbrance",95,56,0},
